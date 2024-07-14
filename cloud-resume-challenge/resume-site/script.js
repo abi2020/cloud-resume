c8e147f6c43bda270d3629fe653a0582d524a23b
Abishek <abisheksiva10@gmail.com>
2024-07-14 16:54:17 -0700

    fixed onload function

--- cloud-resume-challenge/resume-site/script.js
+++ cloud-resume-challenge/resume-site/script.js
@@ -45,18 +45,20 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-window.onscroll = function() {stickyNavbar()};
+window.onload = function() {
+    window.onscroll = function() {stickyNavbar()};
 
-var navbar = document.getElementById("navbar");
-var section = document.querySelector('.section');
-var sticky = navbar.offsetTop;
+    var navbar = document.getElementById("navbar");
+    var section = document.querySelector('.section');
+    var sticky = navbar.offsetTop;
 
-function stickyNavbar() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-    section.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-    section.classList.remove("sticky");
-  }
+    function stickyNavbar() {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky");
+            section.classList.add("sticky");
+        } else {
+            navbar.classList.remove("sticky");
+            section.classList.remove("sticky");
+        }
+    }
 }
