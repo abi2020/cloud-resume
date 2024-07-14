61a2eb4c182feb8b5eaf9f39bd5ed681275bfc5e
Abishek <abisheksiva10@gmail.com>
2024-07-14 15:44:05 -0700

    added nav bar and background

--- cloud-resume-challenge/resume-site/script.js
+++ cloud-resume-challenge/resume-site/script.js
@@ -11,3 +11,52 @@ function fetchData() {
             document.getElementById('replaceme').innerText = 'Error loading data';
         });
 }
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var links = document.querySelectorAll("nav a");
+
+    links.forEach(function(link) {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+
+            links.forEach(function(link) {
+                link.classList.remove("active");
+            });
+
+            link.classList.add("active");
+
+            if (link.classList.contains("active")) {
+                var targetId = link.getAttribute("href").substring(1);
+                var targetElement = document.getElementById(targetId);
+                
+                const offset = 85;
+                const bodyRect = document.body.getBoundingClientRect().top;
+                const elementRect = targetElement.getBoundingClientRect().top;
+                const elementPosition = elementRect - bodyRect;
+                const offsetPosition = elementPosition - offset;
+
+                window.scrollTo({
+                    top: offsetPosition,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
+
+window.onscroll = function() {stickyNavbar()};
+
+var navbar = document.getElementById("navbar");
+var section = document.querySelector('.section');
+var sticky = navbar.offsetTop;
+
+function stickyNavbar() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+    section.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+    section.classList.remove("sticky");
+  }
+}
