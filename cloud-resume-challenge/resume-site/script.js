5443a75253ab24449b2e07948cc70231e012b3dd
Abishek <abisheksiva10@gmail.com>
2024-08-02 09:11:18 +0530

    fixed jumping nav bar

--- cloud-resume-challenge/resume-site/script.js
+++ cloud-resume-challenge/resume-site/script.js
@@ -49,16 +49,13 @@ window.onload = function() {
     window.onscroll = function() {stickyNavbar()};
 
     var navbar = document.getElementById("navbar");
-    var section = document.querySelector('.section');
     var sticky = navbar.offsetTop;
 
     function stickyNavbar() {
         if (window.pageYOffset >= sticky) {
             navbar.classList.add("sticky");
-            section.classList.add("sticky");
         } else {
             navbar.classList.remove("sticky");
-            section.classList.remove("sticky");
         }
     }
 }
